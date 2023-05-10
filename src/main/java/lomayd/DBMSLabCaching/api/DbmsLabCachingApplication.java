b68fcbf2f6147ce268c9de44397724d4c26de8dd
lomayd <jinho10167@gmail.com>
2023-05-10 04:26:04 +0000

    feat: Project Initialize

---
+++ src/main/java/lomayd/DBMSLabCaching/api/DbmsLabCachingApplication.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabCaching.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsLabCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsLabCachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/DBMSLabCaching/DbmsLabCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabCaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsLabCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
